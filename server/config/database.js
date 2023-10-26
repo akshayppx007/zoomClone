933698794a3dd7e272380522524ba28ec10a848a
akshayppx007 <akshayppx007@gmail.com>
2023-10-26 12:23:14 +0530

    minor update

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 const catchAsyncErrors = require("../utils/catchAsyncErrors");
 
-const connectDatabase = catchAsyncErrors(async () => {
-  const connect = await mongoose.connect(process.env.DB_URI);
-  console.log("Database connected successfully");
-});
+const connectDatabase = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.DB_URI);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = connectDatabase;
